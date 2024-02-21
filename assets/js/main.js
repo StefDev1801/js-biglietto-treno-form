204484ccb558c007f4ad42836af1f160126aa3a1
StefDev1801 <stefano200118@gmail.com>
2024-02-21 09:24:35 +0100

    fix random numbers

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,8 +3,18 @@
 // Voglio che kmToTravel mi dia il valore che scrivo quando scrivo dentro al mio input id="km"
 let elementKmToTravel = document.getElementById('km');//html input dei km
 let elementAgeCostumer = document.getElementById('age');//html input dell età
+let nome = document.getElementById('name_surname');
 console.log(elementKmToTravel);
 console.log(elementAgeCostumer);
+console.log(nome);
+
+//I want to show random nubers from 1 to 9
+console.log(Math.floor(Math.random() * 10));
+let carrozza  = Math.floor(Math.random() * 10);
+
+//I want to show random 4 digits numbers
+console.log(Math.floor(Math.random() * 10000) + 1000);
+let codice = Math.floor(Math.random() * 10000) + 1000;
 
 //<!-- - avere una variabile con il prezzo (€0.21/km) -->
 //- SE il passeggiero ha 17 anni o meno applicare lo sconto del 20%
@@ -23,18 +33,30 @@ element.addEventListener('click', function() {
      if (elementAgeCostumer < 18) {                         
           ticketPrice = (((elementKmToTravel*0.21)/100*80).toFixed(2));
           console.log("Lei dovrà pagare €" + ticketPrice);
+document.querySelector('#train_carriage').innerHTML= carrozza;
+document.querySelector('#codiceCP').innerHTML=  codice;
+
+
      }
      //    - OPPURE SE il passeggiero ha più o uguale a 65 anni applicare lo sconto del 40%
      else if(elementAgeCostumer >= 65){                    
           ticketPrice = (((elementKmToTravel*0.21)/100*60).toFixed(2));
           console.log("Lei dovrà pagare €" + ticketPrice);
+document.querySelector('#train_carriage').innerHTML= carrozza;
+document.querySelector('#codiceCP').innerHTML=  codice;
+
+
      }
      //        - OPPURE far pagare prezzo pieno
      else{                                          
           ticketPrice = ((elementKmToTravel*0.21).toFixed(2));
           console.log("Lei dovrà pagare €" + ticketPrice);
-     }
+document.querySelector('#train_carriage').innerHTML= carrozza;
+document.querySelector('#codiceCP').innerHTML=  codice;
 
+
+     }
+     
 });
 //numero carrozza
 
@@ -46,19 +68,22 @@ cancel.addEventListener('click', function () {
 })
 */
 
-//I want to show random nubers from 1 to 9
-console.log(Math.floor(Math.random() * 10));
-let carrozza  = Math.floor(Math.random() * 10);
-document.querySelector('#train_carriage').innerHTML= carrozza;
-
-
-//I want to show random 4 digits numbers
-console.log(Math.floor(Math.random() * 10000) + 1000);
-let codice = Math.floor(Math.random() * 10000) + 1000;
-document.querySelector('#codiceCP').innerHTML=  codice;
 
 
 
+
+/* 
+//I want to read an imput that a costumer gave me and write it in another place of HTML
+//see the html inside of javascript with a console.log
+console.log(document.querySelector('.name_surname'));//così non vedo niente dato che la pagina inizialmente è vuota
+//when i click a button i want to copy it and put it there
+let nome = document.querySelector('.name_surname');
+console.log(nome);
+nome.addEventListener('click', function(){
+
+})
+//to put it there i need a variable tath can save the name
 
 //- Far visualizzare l'output desiderato con massimo due cifre decimali e il simbolo €
 // document.writeln, creare una sola variabile per accorpare il prezzo?
+*/
